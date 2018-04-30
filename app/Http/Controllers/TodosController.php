91e02cc277bb73002d4b105c9ffdceea761584a1
matt1007k <matt1007k@gmail.com>
2018-04-30 15:59:51 -0500

    Api con laravel de aplicación de escritorio con React y Electron

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+
+class TodosController extends Controller
+{
+    public function index(){
+        $todos = Todo::all();
+        return $response()->json([
+            'todos' => $todos
+        ], 200);
+    }
+
+    public function store(Request $request){
+        return Todo::create([
+            'content' => $request->content,
+            'completed' => false
+        ], 200);
+    }
+
+    public function delete(Request $request){
+        $todo = Todo::whereId($request->id)->first();
+        if(!is_null($todo)){
+            $todo->delete();
+        }
+        return response(200);
+    }
+
+    public function complete(Request $request){
+        $todo = Todo::whereId($request->id)->first();
+
+        if (!is_null($todo)){
+            $todo->completed = !$todo->completed;
+            $todo->save();
+        }
+
+        return response(200);
+    }
+}
